2b853516ac6637759426b051880683fcedf3a3a9
Harsimran <harsimrans329@gmail.com>
2016-03-01 00:04:56 -0600

    Http Url Connection is made

--- app/src/main/java/com/cineflick/developer/harry/MainActivity.java
+++ app/src/main/java/com/cineflick/developer/harry/MainActivity.java
@@ -2,19 +2,29 @@ package com.cineflick.developer.harry;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import com.cineflick.developer.harry.utils.AppConstants;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG =MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +32,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if(isNetworkAvailable()){
+            //Start background Thread
+            new MovieQuerryClass().execute();
 
-        //Check Network state
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if(networkInfo != null && networkInfo.isConnected() != false){
 
-        //To do background networking stuff
-
-        }else{
-            Toast.makeText(this,"No network is available",Toast.LENGTH_LONG).show();
         }
-
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +48,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private boolean isNetworkAvailable(){
+        //Check Network state
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected() != false){
+            Log.d(TAG,getResources().getString(R.string.network_availbale));
+            return true;
+        }else{
+            Log.d(TAG,getResources().getString(R.string.network_not_availbale));
+            return false;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -65,5 +80,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class MovieQuerryClass extends AsyncTask<Void, Void, Void> {
+
+        protected Void doInBackground(Void... params) {
+            String urlWithAppKey = AppConstants.URL+AppConstants.POPULARITY_DESC+AppConstants.API_KEY;
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String movieJsonStr = null;
+            try{
+                URL url = new URL(urlWithAppKey);
+                urlConnection =(HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream =urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null){
+                    Log.e(TAG,"Input stream is null");
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while((line=reader.readLine())!=null){
+                    buffer.append(line +"\n");
+                }
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                movieJsonStr = buffer.toString();
+                Log.d(TAG,movieJsonStr);
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
     }
 }
---
+++ app/src/main/java/com/cineflick/developer/harry/utils/AppConstants.java
@@ -0,0 +1,15 @@
+package com.cineflick.developer.harry.utils;
+
+/**
+ * Created by harry on 2/29/16.
+ */
+public class AppConstants {
+
+    //url for movie db and api key
+    public static final String URL= "https://api.themoviedb.org/3/discover/movie?sort_by=";
+    public static final String POPULARITY_DESC="popularity.desc&api_key=";
+    public static final String RATING_DESC="vote_average.gte=3.5&api_key";
+    public static final String API_KEY="";
+
+}
+
